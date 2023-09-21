2baebec47295ae123e64a48216b13674326dcaa8
Oshoaro Favour <oshoarofavour@gmail.com>
2023-09-21 12:20:57 +0100

    done with the styling of the left section component

--- src/components/LeftSection.jsx
+++ src/components/LeftSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { LuMusic2 } from "react-icons/lu";
-import { CiSearch } from "react-icons/ci";
+import { PiMusicNoteDuotone } from "react-icons/pi";
+import { BiSearchAlt } from "react-icons/bi";
 import { AiFillHome } from "react-icons/ai";
 import { MdOutlinePlaylistAddCheckCircle } from "react-icons/md";
 import { FaHeadphonesAlt } from "react-icons/fa";
@@ -8,64 +8,78 @@ import { GiLoveSong } from "react-icons/gi";
 import { IoAlbumsOutline } from "react-icons/io5";
 import { BsGear } from "react-icons/bs";
 import { BiLogOut } from "react-icons/bi";
+import { AiOutlineVideoCamera } from "react-icons/ai";
 
 const LeftSection = () => {
-    return (
-        // #AF3143
-        <section>
-            <div className="flex items-center">
-                <LuMusic2 className="text-[#B23238] text-3xl"/>
-                <h2 className="text-2xl">Music</h2>
-            </div>
-            <div>
-                <CiSearch />
-                <input type="text" placeholder="Search" />
-            </div>
+  return (
+    <section className="w-[22%] h-screen border border-r-white border-l-0 border-t-0 border-b-0">
+      <div className="flex flex-col items-center pt-5">
+        <div className="flex items-center">
+          <PiMusicNoteDuotone className="text-[#B23238] text-4xl" />
+          <h2 className="text-2xl pl-2">Music</h2>
+        </div>
+        <div className="flex items-center border border-gray-50 rounded-full w-[70%] h-9 px-2 mt-10">
+          <i>
+            <BiSearchAlt className="text-xl cursor-pointer" />
+          </i>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-transparent outline-none ml-2 overflow-x-auto"
+          />
+        </div>
+      </div>
 
-            {/* Menu */}
-            <div>
-                <h2>Menu</h2>
-                <span>
-                    <AiFillHome />
-                    <p>Home</p>
-                </span>
-                <span>
-                    <MdOutlinePlaylistAddCheckCircle />
-                    <p>Playlists</p>
-                </span>
-                <span>
-                    <FaHeadphonesAlt />
-                    <p>Artists</p>
-                </span>
-            </div>
+      <div className="pl-12 space-y-10 mt-5">
+        {/* Menu */}
+        <div className="space-y-3">
+          <h2 className="text-xl">Menu</h2>
+          <span className="flex items-center cursor-pointer">
+            <AiFillHome className="text-lg" />
+            <p className="text-base pl-2">Home</p>
+          </span>
+          <span className="flex items-center cursor-pointer">
+            <MdOutlinePlaylistAddCheckCircle className="text-lg" />
+            <p className="text-base pl-2">Playlists</p>
+          </span>
+          <span className="flex items-center cursor-pointer">
+            <FaHeadphonesAlt className="text-lg" />
+            <p className="text-base pl-2">Artists</p>
+          </span>
+        </div>
 
-            {/* Library */}
-            <div>
-                <h2>Library</h2>
-                <span>
-                    <GiLoveSong />
-                    <p>Songs</p>
-                </span>
-                <span>
-                    <IoAlbumsOutline />
-                    <p>Albums</p>
-                </span>
-            </div>
+        {/* Library */}
+        <div className="space-y-3">
+          <h2 className="text-xl">Library</h2>
+          <span className="flex items-center cursor-pointer">
+            <GiLoveSong className="text-lg" />
+            <p className="text-base pl-2">Songs</p>
+          </span>
+          <span className="flex items-center cursor-pointer">
+            <IoAlbumsOutline className="text-lg" />
+            <p className="text-base pl-2">Albums</p>
+          </span>
+          <span className="flex items-center cursor-pointer">
+            <AiOutlineVideoCamera className="text-lg" />
+            <p className="text-base pl-2">Music Videos</p>
+          </span>
+        </div>
 
-            {/* Other */}
-            <div>
-                <h2>Other</h2>
-                <span>
-                    <BsGear />
-                    <p>Settings</p>
-                </span>
-                <span>
-                    <BiLogOut />
-                    <p>Log out</p>
-                </span>
-            </div>
-        </section>
-    );
+        {/* Other */}
+        <div className="space-y-3">
+          <h2 className="text-xl">Other</h2>
+          <span className="flex items-center cursor-pointer">
+            <BsGear className="text-lg" />
+            <p className="text-base pl-2">Settings</p>
+          </span>
+          <span className="flex items-center cursor-pointer">
+            <BiLogOut className="text-lg" />
+            <p className="text-base pl-2">Log out</p>
+          </span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default LeftSection;
