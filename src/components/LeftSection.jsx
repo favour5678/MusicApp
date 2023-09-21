4430e1fc63c7efcf6342592618bd8fe5285533e1
Oshoaro Favour <oshoarofavour@gmail.com>
2023-09-21 10:40:35 +0100

    done writing the html for the left section

--- src/components/LeftSection.jsx
+++ src/components/LeftSection.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import { LuMusic2 } from 'react-icons/lu'
-import { CiSearch } from 'react-icons/ci'
-import { AiFillHome } from 'react-icons/ai'
-import { MdOutlinePlaylistAddCheckCircle } from 'react-icons/md'
+import React from "react";
+import { LuMusic2 } from "react-icons/lu";
+import { CiSearch } from "react-icons/ci";
+import { AiFillHome } from "react-icons/ai";
+import { MdOutlinePlaylistAddCheckCircle } from "react-icons/md";
+import { FaHeadphonesAlt } from "react-icons/fa";
+import { GiLoveSong } from "react-icons/gi";
+import { IoAlbumsOutline } from "react-icons/io5";
+import { BsGear } from "react-icons/bs";
+import { BiLogOut } from "react-icons/bi";
 
 const LeftSection = () => {
-  return (
+    return (
         <section>
             <div>
                 <LuMusic2 />
@@ -13,19 +18,53 @@ const LeftSection = () => {
             </div>
             <div>
                 <CiSearch />
-                <input type="text" placeholder='Search'/>
+                <input type="text" placeholder="Search" />
             </div>
-            <h2>Menu</h2>
+
+            {/* Menu */}
             <div>
-                <AiFillHome />
-                <p>Home</p>
+                <h2>Menu</h2>
+                <span>
+                    <AiFillHome />
+                    <p>Home</p>
+                </span>
+                <span>
+                    <MdOutlinePlaylistAddCheckCircle />
+                    <p>Playlists</p>
+                </span>
+                <span>
+                    <FaHeadphonesAlt />
+                    <p>Artists</p>
+                </span>
             </div>
+
+            {/* Library */}
             <div>
-                <MdOutlinePlaylistAddCheckCircle />
-                <p>Playlists</p>
+                <h2>Library</h2>
+                <span>
+                    <GiLoveSong />
+                    <p>Songs</p>
+                </span>
+                <span>
+                    <IoAlbumsOutline />
+                    <p>Albums</p>
+                </span>
+            </div>
+
+            {/* Other */}
+            <div>
+                <h2>Other</h2>
+                <span>
+                    <BsGear />
+                    <p>Settings</p>
+                </span>
+                <span>
+                    <BiLogOut />
+                    <p>Log out</p>
+                </span>
             </div>
         </section>
-  )
-}
+    );
+};
 
-export default LeftSection
+export default LeftSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import LeftSection from "../components/LeftSection";
 import RightSection from "../components/RightSection";
 
-
 const Home = () => {
   return (
     <main className="bg-gradient-to-br from from-[#3B1D26] to-[#111111] w-full h-screen text-white">
       <div>
         <LeftSection />
-        <RightSection />
+        {/* <RightSection /> */}
       </div>
     </main>
   );
