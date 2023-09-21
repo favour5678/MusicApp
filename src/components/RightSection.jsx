cf7cefd69b78a26e171bcb18d6842860e3413589
Oshoaro Favour <oshoarofavour@gmail.com>
2023-09-21 16:01:03 +0100

    made a change & working on the api

--- src/components/RightSection.jsx
+++ src/components/RightSection.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const RightSection = () => {
+  const [musicData, setMusicData] = useState([])
+  
+  useEffect(() => {
+    const apiUrl = 'https://robo-music-api.onrender.com/music/my-api'
+
+    axios.get(apiUrl)
+      .then(response => {
+        setMusicData(response.data)
+        console.log(response.data)
+      }) .catch(error => {
+        console.log('Error fetching music data', error);
+      })
+    }, [])
+
+
   return (
     <section className='w-[78%] h-screen overflow-y-auto'>
+      {musicData.map(song => (
+        <div key={song.id}>
+          {song.id}
+        </div>
+      ))}
     </section>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import RightSection from "../components/RightSection";
 
 const Home = () => {
   return (
-    <main className="bg-gradient-to-br from from-[#3B1D26] to-[#111111] w-full h-screen text-white font-body">
+    <main className="bg-gradient-to-br from from-[#3B1D26] to-[#111111] w-full h-screen text-gray-200 font-body">
       <div className="container max-w-full flex">
         <LeftSection />
         <RightSection />
